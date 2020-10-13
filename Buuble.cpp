5181ae329e759aa2b0708d0d00f9a3a1f4b3366b
aditya99prakash <36444408+aditya99prakash@users.noreply.github.com>
2020-10-13 21:03:14 +0530

    Update Buuble.cpp

--- Buuble.cpp
+++ Buuble.cpp
@@ -1,25 +1,23 @@
-void swap(int *xp, int *yp)  
-{  
-    int temp = *xp;  
-    *xp = *yp;  
-    *yp = temp;  
-}  
 
 void bubbleSort(int arr[], int n)  
 {  
-    int i, j;  
+    int i, j,tmp;  
     for (i = 0; i < n-1; i++)      
       
 
     for (j = 0; j < n-i-1; j++)  
         if (arr[j] > arr[j+1])  
-            swap(&arr[j], &arr[j+1]);  
+        {           tmp=arr[j]; //swaoping
+             arr[j]=arr[j+1];
+             arr[j+1]=arr[j];
+       }
 }  
 
 void printArray(int arr[], int size)  
 {  
     int i;  
     for (i = 0; i < size; i++)  
+        cout<<"SORTED ARRAY IS ";
         cout << arr[i] << " ";  
     cout << endl;  
 }  
